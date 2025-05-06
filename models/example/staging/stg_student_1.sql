4f749b6920250e9c043e1bd59e0c63bc8f398be5
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2025-05-06 17:51:30 +0530

    Create stg_student_1.sql

---
+++ models/example/staging/stg_student_1.sql
@@ -0,0 +1,16 @@
+WITH
+student AS (
+SELECT * FROM `handy-coil-457005-r4.bk_dataset.student_1`),
+
+ae AS ( 
+SELECT 
+    student_id,
+    first_name,
+    last_name,
+    age,
+    email,
+    enrollment_date,
+    created_at,
+    updated_at
+FROM student
+)
