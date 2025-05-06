8ea62d87d1da7b53a101e02f1ee2d8c3230d01a4
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2025-05-06 17:50:55 +0530

    Create inter_student.sql

---
+++ models/example/intermediate/inter_student.sql
@@ -0,0 +1,31 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'student_id',
+    incremental_strategy = 'merge',
+    partition_by={
+        "field": "enrollment_date",
+        "data_type": "date",
+        "granularity": "year"
+    }
+) }}
+
+with source_data as (
+    select
+        s1.student_id,
+        s1.first_name,
+        s1.last_name,
+        s1.age,
+        s1.email,
+        s1.enrollment_date,
+        s1.created_at,
+        s2.course_id,
+        s2.course_name,
+        s2.credits,
+        s2.grade,s1.updated_at
+    from {{ ref('stg_student_1') }} s1
+    inner join {{ ref('stg_student_2') }} s2
+        on s1.student_id = s2.student_id
+)
+
+select *
+from source_data
